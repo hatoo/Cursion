ea69691e96e98adbb2a48ba9b6a099d4a01cf357
hatoo <hato2000@gmail.com>
2019-05-13 23:16:58 +0900

    DiffStyle

--- src/style.rs
+++ src/style.rs
@@ -2,9 +2,15 @@ use std::fmt;
 
 #[derive(Debug, Eq, PartialEq, Clone, Default)]
 pub struct Style {
-    fg: crate::color::TermColor,
-    bg: crate::color::TermColor,
-    decorations: crate::decoration::Decorations,
+    pub fg: crate::color::TermColor,
+    pub bg: crate::color::TermColor,
+    pub decorations: crate::decoration::Decorations,
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct DiffStyle<'a, 'b> {
+    pub from: &'a Style,
+    pub to: &'b Style,
 }
 
 impl fmt::Display for Style {
@@ -12,6 +18,25 @@ impl fmt::Display for Style {
         termion::color::Fg(self.fg).fmt(f)?;
         termion::color::Bg(self.bg).fmt(f)?;
         self.decorations.fmt(f)?;
+
+        Ok(())
+    }
+}
+
+impl<'a, 'b> fmt::Display for DiffStyle<'a, 'b> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.from.fg != self.to.fg {
+            termion::color::Fg(self.to.fg).fmt(f)?;
+        }
+
+        if self.from.bg != self.to.bg {
+            &termion::color::Bg(self.to.bg).fmt(f)?;
+        }
+
+        if self.from.decorations != self.to.decorations {
+            termion::style::Reset.fmt(f)?;
+            self.to.decorations.fmt(f)?;
+        }
 
         Ok(())
     }
