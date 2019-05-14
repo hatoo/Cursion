f98db7b2c58fb233e73ef9544df7fed7f1b7272a
hatoo <hato2000@gmail.com>
2019-05-14 22:49:34 +0900

    TermWriter

--- src/term.rs
+++ src/term.rs
@@ -10,6 +10,7 @@ enum Tile {
     Char(char, Style),
 }
 
+#[derive(Debug)]
 pub struct Term {
     height: usize,
     width: usize,
@@ -54,7 +55,7 @@ impl Term {
                 return Ok(());
             }
 
-            if col + w >= self.width {
+            if col + w > self.width {
                 return Err(Error::NotEnoughSpace);
             }
 
@@ -83,6 +84,76 @@ impl Term {
             }
         }
         Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct TermWriter<'a> {
+    term: &'a mut Term,
+    row: usize,
+    col: usize,
+}
+
+impl<'a> TermWriter<'a> {
+    pub fn new(term: &'a mut Term) -> Self {
+        Self {
+            term,
+            row: 0,
+            col: 0,
+        }
+    }
+
+    pub fn is_writable(&self) -> bool {
+        self.row < self.term.height
+    }
+
+    pub fn newline(&mut self) {
+        self.row += 1;
+    }
+
+    pub fn cause_newline(&self, ch: char) -> bool {
+        if let Some(w) = ch.width() {
+            self.col + w > self.term.width
+        } else {
+            false
+        }
+    }
+
+    pub fn write(&mut self, ch: char, style: Style) -> Option<(usize, usize)> {
+        if !self.is_writable() {
+            return None;
+        }
+
+        let w = ch.width()?;
+        if w == 0 {
+            return None;
+        }
+
+        if w > self.term.width {
+            return None;
+        }
+
+        if self.col + w > self.term.width {
+            self.row += 1;
+            self.col = 0;
+        }
+
+        if !self.is_writable() {
+            return None;
+        }
+
+        let res = (self.row, self.col);
+        self.term
+            .set_char_at(self.row, self.col, ch, style)
+            .unwrap();
+
+        self.col += w;
+        if self.col >= self.term.width {
+            self.col = 0;
+            self.row += 1;
+        }
+
+        Some(res)
     }
 }
 
@@ -138,5 +209,32 @@ fn test_set_char_at() {
             Tile::Char('a', Style::default()),
             Tile::Empty
         ]]
+    );
+}
+
+#[test]
+fn test_term_writer() {
+    let mut t = Term::new(2, 3);
+    {
+        let mut w = TermWriter::new(&mut t);
+        assert_eq!(w.write('a', Style::default()), Some((0, 0)));
+        assert_eq!(w.write('あ', Style::default()), Some((0, 1)));
+        assert_eq!(w.write('い', Style::default()), Some((1, 0)));
+        assert_eq!(w.write('b', Style::default()), Some((1, 2)));
+    }
+    assert_eq!(
+        t.buf,
+        vec![
+            vec![
+                Tile::Char('a', Style::default()),
+                Tile::Char('あ', Style::default()),
+                Tile::Occupied,
+            ],
+            vec![
+                Tile::Char('い', Style::default()),
+                Tile::Occupied,
+                Tile::Char('b', Style::default()),
+            ]
+        ]
     );
 }
