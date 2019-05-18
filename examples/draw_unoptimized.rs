ef0e816de7c23a2b0ec783cba1cb2784f881d0f9
hatoo <hato2000@gmail.com>
2019-05-18 19:02:27 +0900

    Update exmaple

--- examples/draw_unoptimized.rs
+++ examples/draw_unoptimized.rs
@@ -1,21 +1,28 @@
 use cursion::style::Style;
 use cursion::term::{Term, TermWriter};
 use std::io::{stdout, Write};
+use std::thread;
+use std::time;
 use termion::input::MouseTerminal;
 use termion::raw::IntoRawMode;
 
 fn main() {
     let mut stdout = MouseTerminal::from(stdout()).into_raw_mode().unwrap();
-    let mut t = Term::with_terminal_size().unwrap();
-    {
-        let mut w = TermWriter::new(&mut t);
-        for i in 0..100 {
-            if i % 25 == 0 {
-                w.newline();
+
+    for i in 0..120 {
+        let mut t = Term::with_terminal_size().unwrap();
+        {
+            let mut w = TermWriter::new(&mut t);
+            for j in 0..120 {
+                if j % 25 == 0 {
+                    w.newline();
+                }
+                w.write(if i == j { 'a' } else { 'b' }, Style::default())
+                    .unwrap();
             }
-            w.write('a', Style::default()).unwrap();
         }
+        t.draw(&mut stdout).unwrap();
+        stdout.flush().unwrap();
+        thread::sleep(time::Duration::from_secs(1) / 120);
     }
-    t.draw(&mut stdout).unwrap();
-    stdout.flush().unwrap();
 }
