c1a6821d3b14866a18f4c218a7edb2cb54253bf0
hatoo <hato2000@gmail.com>
2019-05-18 20:45:20 +0900

    Update exmaple

--- examples/draw_unoptimized.rs
+++ examples/draw_unoptimized.rs
@@ -3,11 +3,9 @@ use cursion::term::{Term, TermWriter};
 use std::io::{stdout, Write};
 use std::thread;
 use std::time;
-use termion::input::MouseTerminal;
-use termion::raw::IntoRawMode;
 
 fn main() {
-    let mut stdout = MouseTerminal::from(stdout()).into_raw_mode().unwrap();
+    let mut stdout = stdout();
 
     for i in 0..240 {
         let mut t = Term::with_terminal_size().unwrap();
