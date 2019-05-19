4439c48a3e9a0e6ff59e413146adbedc9f6198de
hatoo <hato2000@gmail.com>
2019-05-19 16:18:01 +0900

    fix

--- src/style.rs
+++ src/style.rs
@@ -24,9 +24,9 @@ pub struct DiffStyle {
 
 impl fmt::Display for Style {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.decorations.fmt(f)?;
         termion::color::Fg(self.fg).fmt(f)?;
         termion::color::Bg(self.bg).fmt(f)?;
-        self.decorations.fmt(f)?;
 
         Ok(())
     }
@@ -34,16 +34,16 @@ impl fmt::Display for Style {
 
 impl fmt::Display for DiffStyle {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.from.decorations != self.to.decorations {
+            self.to.decorations.fmt(f)?;
+        }
+
         if self.from.fg != self.to.fg {
             termion::color::Fg(self.to.fg).fmt(f)?;
         }
 
         if self.from.bg != self.to.bg {
             termion::color::Bg(self.to.bg).fmt(f)?;
-        }
-
-        if self.from.decorations != self.to.decorations {
-            self.to.decorations.fmt(f)?;
         }
 
         Ok(())
